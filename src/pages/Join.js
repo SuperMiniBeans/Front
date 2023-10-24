288ff89d6de28d9467aff85f64a5a2ca5543be73
song-tiigger <applej07@naver.com>
2023-10-24 23:16:02 +0900

    [ADD] 주소검색 수정

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -2,34 +2,33 @@ import styled from "styled-components";
 import { useState } from "react";
 import axios from "axios";
 // import { response } from "express";
-import Post from "../components/Post";
 import { CompleteBtn } from "../styles/ButtonStyle";
 import { FlexBox } from "../styles/Layout";
+import Post from "../components/Post";
 
 function Join() {
+  // 사용자 정보
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
-  // const [postcode, setPostcode] = useState("");
-  // const [address, setAddress] = useState("");
   const [enroll_company, setEnroll_company] = useState({
     postcode: '',
-    address: '',
+    address1: '',
+    address3: '',
   });
-  const [detailAddress, setDetailAddress] = useState("");
-  const [etcAddress, setEtcAddress] = useState("");
+  const [address2, setAddress2] = useState("");
   const [phoneNumber, setphoneNumber] = useState("");
 
+  // 에러 메세지
   const [emailMessage, setEmailMessage] = useState("");
   const [passwordMessage, setPasswordMessage] = useState("");
   const [passwordConfirmMessage, setPasswordConfirmMessage] = useState("");
   const [phoneNumMessage, setphoneNumMessage] = useState("");
-
+  // const [msgColor, setMsgColor] = useState("green");
 
   // 주소 검색창 팝업
   const [popup, setPopup] = useState(false);
-
 
   // 사용자 이름 받기
   const userName = e => {
@@ -49,17 +48,16 @@ function Join() {
   }
 
   // 이메일 중복 체크
-  // function checkEmail() {
-  //   console.log(e.target.value);
-  //   axios.get(' ')
-  //     .then(response => {
-  //       if(response === false) {
-  //         alert('사용 가능한 아이디입니다');
-  //       } else {
-  //         alert('이미 사용중인 아이디입니다');
-  //       }
-  //     })
-  // }
+  function checkEmail() {
+    axios.get('/join')
+      .then(response => {
+        if(response.data === 1) {
+          alert('이미 사용중인 아이디입니다');
+        } else {
+          alert('사용 가능한 아이디입니다');
+        }
+      })
+  }
 
   // 사용자 비밀번호 받기, 유효성 검사
   const userPassword = e => {
@@ -87,21 +85,18 @@ function Join() {
   // 주소 검색 관련
   const handleInput = (e) => {
     setEnroll_company({
-      ...enroll_company,
-      [e.target.name]: e.target.value,
-    })
+        ...enroll_company,
+          [e.target.name]:e.target.value,
+      })
   }
   
   const handleComplete = (data) => {
     setPopup(!popup);
   }
 
-  const userDetailAddress = e => {
-    setDetailAddress(e.target.value);
-  }
-
-  const userEtcAddress = e => {
-    setEtcAddress(e.target.value);
+  // 사용자 상세주소 받기
+  const userAddress2 = e => {
+    setAddress2(e.target.value);
   }
 
   // 사용자 연락처 받기
@@ -118,26 +113,43 @@ function Join() {
 
   // 회원가입 버튼 클릭시 실행될 작업
   function SignUp() {
-    axios.post('/join', {
-      userName: name,
-      userEmail: email,
-      userPassword: password,
-      address: enroll_company,
-      userPhoneNumber: phoneNumber,
-    })
-      .then(response => {
-        alert(response.status + " 회원가입이 완료되었습니다.");
-        console.log(name, email, password, phoneNumber);
-      }).catch(error => {
-        alert(error);
-        console.log(name, email, password, phoneNumber);
-      });
+    if (
+      //입력칸 공백 확인
+      name === "" ||
+      email === "" ||
+      password === "" ||
+      passwordConfirm === "" ||
+      enroll_company.postcode === "" ||
+      enroll_company.address1 === "" ||
+      address2 === "" ||
+      phoneNumber === "" 
+    ) {
+      alert("정보를 입력해주세요.");
+    } else {
+      axios.post('/join', {
+        userName: name,
+        userEmail: email,
+        userPassword: password,
+        userAddressNumber: enroll_company.postcode,
+        userAddress1: enroll_company.address1,
+        userAddress2: address2,
+        userAddress3: enroll_company.address3,
+        userPhoneNumber: phoneNumber,
+      })
+        .then(response => {
+          alert(response.status + " 회원가입이 완료되었습니다.");
+          console.log('성공','이름:'+ name, '이메일:'+email,'비밀번호:'+password, '우편번호:'+enroll_company.postcode, '주소:'+enroll_company.address1, '상세주소:'+address2, '참고항목:'+enroll_company.address3, '휴대폰 번호:'+phoneNumber);
+        }).catch(error => {
+          alert(error);
+          console.log('실패', '이름:'+ name, '이메일:'+email,'비밀번호:'+password, '우편번호:'+enroll_company.postcode, '주소:'+enroll_company.address1, '상세주소:'+address2, '참고항목:'+enroll_company.address3, '휴대폰 번호:'+phoneNumber);
+        });
+    }
   }
 
   return (
     <JoinWrapper>
       <div className="Join">
-        <h2 style={{textAlign: 'center'}}>회원가입</h2>
+        <TitleCenter><h2>회원가입</h2></TitleCenter>
 
         <FormWrap action="/join" method="POST">
           <ItemWrap>
@@ -145,7 +157,7 @@ function Join() {
               <label>이름</label>
             </div>
             <div>
-              <Input type="text" value={name} placeholder="이름을 입력하세요" onChange={userName}></Input>
+              <Input type="text" name="userName" value={name} placeholder="이름을 입력하세요" onChange={userName}></Input>
             </div>
           </ItemWrap>
 
@@ -155,8 +167,8 @@ function Join() {
             </div>
             <div>
               <FlexBox>
-                <Input type="text" value={email} placeholder="이메일 입력 후 중복확인 버튼을 눌러주세요" onChange={userEmail}></Input>
-                <button style={{width: '90px'}}>중복확인</button>
+                <Input type="text" name="userEmail" value={email} placeholder="이메일 입력 후 중복확인 버튼을 눌러주세요" onChange={userEmail}></Input>
+                <button onClick={checkEmail} style={{width: '90px'}}>중복확인</button>
               </FlexBox>
               <div><span>{emailMessage}</span></div>
             </div>
@@ -168,7 +180,7 @@ function Join() {
             </div>
             <div>
               <Input
-                type="password" value={password} placeholder="영문,숫자를 조합하여 4~8자리로 입력해주세요" maxLength={8} onChange={userPassword} ></Input>
+                type="password" name="userPassword" value={password} placeholder="영문,숫자를 조합하여 4~8자리로 입력해주세요" maxLength={8} onChange={userPassword} ></Input>
               <div><span>{passwordMessage}</span></div>
             </div>
           </ItemWrap>
@@ -179,7 +191,7 @@ function Join() {
             </div>
             <div>
               <Input
-                type="password" value={passwordConfirm} placeholder="비밀번호를 다시 입력해주세요" maxLength={8} onChange={userPasswordConfirm}></Input>
+                type="password" name="userPasswordConfirm" value={passwordConfirm} placeholder="비밀번호를 다시 입력해주세요" maxLength={8} onChange={userPasswordConfirm}></Input>
               <div><span>{passwordConfirmMessage}</span></div>
             </div>
           </ItemWrap>
@@ -190,7 +202,7 @@ function Join() {
             </div>
             <div>
               <FlexBox>
-                <Input type="number" value={enroll_company.postcode} placeholder="우편번호를 검색하세요" onChange={handleInput} readOnly></Input>
+                <Input type="number" name="userAddressNumber" value={enroll_company.postcode} placeholder="우편번호를 검색하세요" onChange={handleInput} readOnly></Input>
                 <button onClick={handleComplete} style={{width: '120px'}}>우편번호 검색</button>
               </FlexBox>
               {popup && <Post company={enroll_company} setcompany={setEnroll_company}></Post>}
@@ -202,7 +214,7 @@ function Join() {
               <label>주소</label>
             </div>
             <div>
-              <Input type="text" value={enroll_company.address} onChange={handleInput} readOnly></Input>
+              <Input type="text" name="userAddress1" value={enroll_company.address1} onChange={handleInput} readOnly></Input>
             </div>
           </ItemWrap>
 
@@ -211,7 +223,7 @@ function Join() {
               <label>상세주소</label>
             </div>
             <div>
-              <Input type="text" value={detailAddress} placeholder="상세주소를 입력해주세요" onChange={userDetailAddress} ></Input>
+              <Input type="text" name="userAddress2" value={address2} placeholder="상세주소를 입력해주세요" onChange={userAddress2} ></Input>
             </div>
           </ItemWrap>
 
@@ -220,7 +232,7 @@ function Join() {
               <label>참고항목</label>
             </div>
             <div>
-              <Input type="text" value={etcAddress} onChange={userEtcAddress}></Input>
+              <Input type="text" name="userAddress3" value={enroll_company.address3} onChange={handleInput}></Input>
             </div>
           </ItemWrap>
           
@@ -229,13 +241,11 @@ function Join() {
               <label>휴대폰 번호</label>
             </div>
             <div>
-              <Input type="text" value={phoneNumber} placeholder="숫자만 입력해주세요" onChange={userPhoneNumber} ></Input>
+              <Input type="text" name="userPhoneNumber" value={phoneNumber} placeholder="숫자만 입력해주세요" onChange={userPhoneNumber} ></Input>
               <div><span>{phoneNumMessage}</span></div>
             </div>
           </ItemWrap>
         </FormWrap>
-
-        <br /><br />
 
         <CompleteBtn type="submit" onClick={SignUp}>회원가입</CompleteBtn>
       </div>
@@ -250,7 +260,12 @@ const JoinWrapper = styled.div`
   padding: 0 20px;
   display: flex;
   flex-direction: column;
-`;
+`
+
+const TitleCenter = styled.div`
+  margin-bottom: 30px;
+  text-align: center;
+`
 
 const FormWrap = styled.div`
   display: flex;
@@ -258,13 +273,12 @@ const FormWrap = styled.div`
 `;
 
 const ItemWrap = styled.div`
-  height: 88px;
+  height: 80px;
 `
 
 const Input = styled.input`
   width: 100%;
   height: 40px;
-  margin-bottom: 4px;
   padding-left: 10px;
   border: 1px solid #aaa;
 `
