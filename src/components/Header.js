33f36261fd62dcd49c9ef6e693d2203021ae8086
song-tiigger <applej07@naver.com>
2023-11-03 01:01:58 +0900

    [ADD]레이아웃 수정 및 주석 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,63 +1,66 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Container, FlexBox } from "../styles/Layout";
+import { FlexBox } from "../styles/Layout";
 import { GoSearch } from "react-icons/go"
 import { SlBag } from "react-icons/sl"
 
 function Header() {
+  // 로그인 상태 확인 여부
+  // const [isLogin, setIsLogin] = useState();
+  /* 로그인 되면 마이페이지 메뉴 보이게, 로그아웃은 마이페이지 옆에 보이게 ?? */
 
+  /* 카테고리 Link to에도 쿼리스트링으로 지정?! 아니면 라우터로 설정? */
+  /* OUTER클릭하면 OUTER에 속한 모든 제품 보이게 하기 */
 
   return(
-    <Container>
-      <HeaderWrap>
-        <HeaderFlexBox>
-          <H1Wrap><h1><Link to="/">LYS</Link></h1></H1Wrap>
+    <HeaderWrap>
+      <HeaderWrapFlex>
+        <H1Wrap><h1><Link to="/">LYS</Link></h1></H1Wrap>
 
-          <GnbUserWrap>
-            <nav className="gnb">
-              <Gnb>
-                <MouseOver>
-                  <Link to="/">OUTER</Link>
-                  <Lnb className="lnb">
-                    <LnbLi><Link to='/'>COAT</Link></LnbLi>
-                    <LnbLi><Link to='/'>JACKET</Link></LnbLi>
-                    <LnbLi><Link to='/'>BLAZERS</Link></LnbLi>
-                  </Lnb>
-                </MouseOver>
-                <MouseOver>
-                  <Link to="/">TOP</Link>
-                  <Lnb className="lnb">
-                    <LnbLi><Link to='/'>T-SHIRTS</Link></LnbLi>
-                    <LnbLi><Link to='/'>SHIRTS</Link></LnbLi>
-                    <LnbLi><Link to='/'>HOODIES/<br />SWEATSHIRTS</Link></LnbLi>
-                    <LnbLi><Link to='/'>KNITWEAR</Link></LnbLi>
-                  </Lnb>
-                </MouseOver>
-                <MouseOver>
-                  <Link to="/">BOTTOM</Link>
-                  <Lnb className="lnb">
-                    <LnbLi><Link to='/'>PANTS</Link></LnbLi>
-                    <LnbLi><Link to='/'>JEANS</Link></LnbLi>
-                  </Lnb>
-                </MouseOver>
-                <MouseOver><Link to="/">ACC</Link></MouseOver>
-                {/* <li><Link to="/">관리자</Link></li> */}
-              </Gnb>
-            </nav>
+        <GnbUserWrap>
+          <nav className="gnb">
+            <Gnb>
+              <MouseOver>
+                <Link to="/productlist">OUTER</Link>
+                <Lnb className="lnb">
+                  <LnbLi><Link to='/'>COAT</Link></LnbLi>
+                  <LnbLi><Link to='/'>JACKET</Link></LnbLi>
+                  <LnbLi><Link to='/'>BLAZERS</Link></LnbLi>
+                </Lnb>
+              </MouseOver>
+              <MouseOver>
+                <Link to="/">TOP</Link>
+                <Lnb className="lnb">
+                  <LnbLi><Link to='/'>T-SHIRTS</Link></LnbLi>
+                  <LnbLi><Link to='/'>SHIRTS</Link></LnbLi>
+                  <LnbLi><Link to='/'>HOODIES/<br />SWEATSHIRTS</Link></LnbLi>
+                  <LnbLi><Link to='/'>KNITWEAR</Link></LnbLi>
+                </Lnb>
+              </MouseOver>
+              <MouseOver>
+                <Link to="/">BOTTOM</Link>
+                <Lnb className="lnb">
+                  <LnbLi><Link to='/'>PANTS</Link></LnbLi>
+                  <LnbLi><Link to='/'>JEANS</Link></LnbLi>
+                </Lnb>
+              </MouseOver>
+              <MouseOver><Link to="/">ACC</Link></MouseOver>
+              {/* <li><Link to="/">관리자</Link></li> */}
+            </Gnb>
+          </nav>
 
-            <UserWrap>
-              <SearchWrap>
-                <div><input type="search"></input></div>
-                <div><button type="submit"><GoSearch /></button></div>
-              </SearchWrap>
-              <div><Link to="/login">Login</Link></div>
-              <ShoppingBag><Link to="/"><SlBag /></Link></ShoppingBag>
-            </UserWrap>
-          </GnbUserWrap>
+          <UserWrap>
+            <SearchWrap>
+              <div><input type="search"></input></div>
+              <div><button type="submit"><GoSearch /></button></div>
+            </SearchWrap>
+            <div><Link to="/login">Login</Link></div>
+            <ShoppingBag><Link to="/"><SlBag /></Link></ShoppingBag>
+          </UserWrap>
+        </GnbUserWrap>
 
-        </HeaderFlexBox>
-      </HeaderWrap>
-    </Container>
+      </HeaderWrapFlex>
+    </HeaderWrap>
   ) 
 }
 
@@ -67,14 +70,16 @@ const HeaderWrap = styled.div`
   width: 100%;
   height: 100px;
   margin-bottom: 100px;
+  border-bottom: 1px solid #000;
   // background-color: skyblue;
 `
 
-const HeaderFlexBox = styled(FlexBox)`
+const HeaderWrapFlex = styled(FlexBox)`
   positoin: relative;
   top:50%;
   transform: translateY(-50%);
   height: 60px;
+  padding: 0 10px;
   align-items: center;
 `
 
@@ -108,7 +113,7 @@ const MouseOver = styled.li`
     display: block;
   }
 `
-/* 디자인.. 최선인가? */
+/* lnb 디자인.. 최선인가? */
 const Lnb = styled.ul `
   display: none;
   position: absolute;
