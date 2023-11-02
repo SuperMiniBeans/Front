d084735b9aca6fe291bb6e491baf6523a0cf4714
song-tiigger <applej07@naver.com>
2023-11-03 01:05:42 +0900

    [ADD]mockdata(상품목록에 사용)

---
+++ src/data/prductMockdata.js
@@ -0,0 +1,109 @@
+let productMockData = [
+  {
+    productNum: 0,
+    title: "상품명0",
+    price: "100000",
+    dscntRate: "50%",
+    dscntPrice: "50000"
+  },
+  {
+    productNum: 1,
+    title: "상품명1",
+    price: "100000",
+    dscntRate: "50%",
+    dscntPrice: "50000"
+  },
+  {
+    productNum: 2,
+    title: "상품명2",
+    price: "100000",
+    dscntRate: "50%",
+    dscntPrice: "50000"
+  },
+  {
+    productNum: 3,
+    title: "상품명3",
+    price: "50000",
+    dscntRate: "50%",
+    dscntPrice: "25000"
+  },
+  {
+    productNum: 4,
+    title: "상품명4",
+    price: "100000",
+    dscntRate: "50%",
+    dscntPrice: "50000"
+  },
+  {
+    productNum: 5,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 6,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 7,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 8,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 9,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 10,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 11,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 12,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 13,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+  {
+    productNum: 14,
+    title: "상품명5",
+    price: "100000",
+    dscntRate: "10%",
+    dscntPrice: "90000"
+  },
+];
+
+export default productMockData;
