3e4e34d3799693cd54df882540afcf13f17dc559
song-tiigger <applej07@naver.com>
2023-11-07 20:59:13 +0900

    [ADD] 로그인 상태관리 추가, 라우터 Main컴포넌트로 이동

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Login from './pages/Login';
-import Join from './pages/Join';
+import { useEffect, useState } from 'react';
 import GlobalStyle from './styles/GlobalStyle';
-import JoinComplete from './pages/JoinComplete';
-import SearchId from './pages/SearchId';
-import ResetPwChk from './pages/ResetPwChk';
-import ResetPw from './pages/ResetPw';
-// import ProductDetail from './components/ProductDetail';
+import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 
 function App() {
-  
+
+  // 사용자 로그인 상태관리
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    if(sessionStorage.getItem("아이디") === null) {
+      console.log('isLogin?', isLogin);
+    } else {
+      console.log('isLogin?', isLogin);
+      setIsLogin(true);
+    }
+  }, [isLogin]);
+
+  // 관리자 로그인 상태관리
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    if(sessionStorage.getItem("아이디") !== 'admin') {
+      console.log('isAdmin?', isAdmin);
+    } else {
+      console.log('isAdmin?', isAdmin);
+      setIsAdmin(true);
+    }
+  }, [isAdmin]);
+
+
   return (
     <div>
       <GlobalStyle />
-      <Header />
+      
+      <Header isLogin={isLogin} isAdmin={isAdmin}/>
       <Main />
-
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/join' element={<Join />} />
-        <Route path='/join/joincomplete' element={<JoinComplete />} />
-        <Route path='/search/id' element={<SearchId />} />
-        <Route path='/search/check/pw' element={<ResetPw />} />
-        <Route path='/search/check' element={<ResetPwChk />} />
-      </Routes>
+      <Footer />
     </div>
   );
 }
