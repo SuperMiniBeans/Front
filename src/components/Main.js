094a68f779b4530da3ae4a65caf473df5c223f09
song-tiigger <applej07@naver.com>
2023-11-09 22:29:14 +0900

    [ADD] 라우터 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+// import Home from "../pages/Home";
 import Login from "../pages/Login";
 import ResetPwChk from "../pages/ResetPwChk";
 import ResetPw from "../pages/ResetPw";
@@ -8,6 +8,7 @@ import SearchId from "../pages/SearchId";
 import Join from "../pages/Join";
 import JoinComplete from "../pages/JoinComplete";
 import MyPage from "../pages/MyPage";
+import Admin from "../pages/Admin";
 // import Product from "../pages/Product";
 
 function Main() {
@@ -15,6 +16,8 @@ function Main() {
   return (
     <div>
       <Routes>
+        <Route path='/' element={<Admin />} />
+
         <Route path='/myPage' element={<MyPage />} />
         {/* <Route path='/product/*' element={<Product />} /> */}
         <Route path='/join/joincomplete' element={<JoinComplete />} />
@@ -23,7 +26,7 @@ function Main() {
         <Route path='/search/check/pw' element={<ResetPw />} />
         <Route path='/search/check' element={<ResetPwChk />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Home />} />
+        {/* <Route path='/' element={<Home />} /> */}
       </Routes>
     </div>
   );
