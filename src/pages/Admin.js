ab8dfc25be5dd1e7edb3c257d9b2a956c248ad48
song-tiigger <applej07@naver.com>
2023-11-25 02:18:39 +0900

    [STYLE] 삭제아이콘 추가, 표 스타일 변경

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,13 +1,20 @@
 import styled from "styled-components";
+import { RiDeleteBin6Line } from "react-icons/ri";
 import { useEffect, useState } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 
 import { Container } from "../styles/Layout";
-import { BtnBg } from "../styles/ButtonStyle";
 import { setProductList } from "../store";
 import AddProduct from "../components/AddProduct"
+
+
+
+// 날짜 처리 어떻게 할지!! 
+// 썸네일 -> 어려우면 패스 (파일 업로드 잘 되면 생각해보기)
+
+
 
 function Admin() {
   const navigate = useNavigate();
@@ -23,14 +30,12 @@ function Admin() {
   console.log(`등록된 상품:`, products);
 
   // DB에 저장된 게시글 불러와서 보여주기
-
   useEffect(() => { 
     axios.post('/fileList', {
       userId: sessionStorage.getItem("아이디"),
     })
       .then(response => {
         console.log('데이터', response.data);
-        console.log(sessionStorage.getItem("아이디"));
         dispatch(setProductList(response.data));
         // if(products.length === 0) {
         //   dispatch(setProductList([]));
@@ -39,9 +44,7 @@ function Admin() {
       })
       .catch(error => {
         console.log(error);
-        console.log(sessionStorage.getItem("아이디"));
         if(products.length === 0) {
-          // dispatch(setProductList([]));
           console.log('데이터 없음');
         }
       })
@@ -55,7 +58,7 @@ function Admin() {
       <Container>
         <h2>상품 관리</h2>
 
-        <div><GoAddProductBtn onClick={goAddProduct}>상품 등록</GoAddProductBtn></div>
+        <div className="add_btn_box"><GoAddProductBtn onClick={goAddProduct}>상품 등록</GoAddProductBtn></div>
 
         <table>
           <caption>등록한 상품 목록</caption>
@@ -72,12 +75,12 @@ function Admin() {
           <thead>
             <tr>
               <th scope="col" id="admin_list_chk"><input type="checkbox"></input></th>
-              <th scope="col" id="admin_list_num">번호</th>
-              <th scope="col" id="admin_list_thumbnail">썸네일</th>
-              <th scope="col" id="admin_list_title">제목</th>
+              <th scope="col" id="admin_list_num">상품 번호</th>
+              {/* <th scope="col" id="admin_list_thumbnail">썸네일</th> */}
+              <th scope="col" id="admin_list_title">상품명</th>
               <th scope="col" id="admin_list_writer">작성자</th>
               <th scope="col" id="admin_list_date">날짜</th>
-              <th scope="col" id="admin_list_del">삭제 버튼</th>
+              <th scope="col" id="admin_list_del">삭제</th>
             </tr>
           </thead>
 
@@ -88,20 +91,18 @@ function Admin() {
                 등록된 상품이 없습니다.
               </td>
             </tbody>
-            // <>
-            //   {window.alert("등록된 상품이 없습니다.")}
-            // </>
           ) : (
             products.map((products, index) => (
               <tbody key={index}>
                 <tr className="tbody_content">
                   <td><input type="checkbox"></input></td>
-                  <td>{index+1}</td>
-                  <td>썸네일</td>
+                  <td>{products.productNumber}</td>
+                  {/* <td>썸네일</td> */}
                   <td><Link to='/product/list/detail'>{products.productName}</Link></td>
-                  <td>admin</td>
-                  <td>{products.today}</td>
-                  <td><button>삭제</button></td>
+                  <td>{products.userId}</td>
+                  <td>{products.productRegisterDate}</td>
+                  {/* <td>2023-11-24</td> */}
+                  <td className="product_del_icon"><RiDeleteBin6Line /></td>
                 </tr>
               </tbody>
             ))
@@ -109,7 +110,7 @@ function Admin() {
           {/* map으로 돌리기 (여기까지-----------)*/}
         </table>
 
-        <div><DeleteProductBtn type="submit">삭제</DeleteProductBtn></div>
+        <div><DeleteChkedBtn type="submit">선택 삭제</DeleteChkedBtn></div>
       </Container>
 
       <Routes>
@@ -126,45 +127,68 @@ const AdminWrap = styled.main`
 
   h2 {
     margin-bottom: 30px;
+    text-align: center;
   }
 
+  // 상품 등록 버튼 영역
+  .add_btn_box {
+    position: relative;
+    height: 40px;
+    margin-bottom: 10px;
+    // background-color: red;
+  }
+
+  // 테이블 스타일 설정
   table,
   th,
   td {
     text-align: center;
-    border: 1px solid #000;
+    // border: none;
+    // border: 1px solid #000;
   }
 
   th,
   td {
     padding: 10px 0;
+    border-bottom: 1px solid #333;
   }
 
   table {
     width: 100%;
+    border-top: 1px solid #333;
 
     caption {
       font-size: 0;
     }
   }
 
+  thead {
+    border-bottom: 1px solid #ccc;
+    font-weight: 600;
+  }
+
   tbody {
     height: 80px;
-    align-items: center;
     // background-color: yellow;
 
-    .tbody_content {
-      align-items: center;
-      line-height: 80px;
-      background-color: #ccc;;
+    td {
+      vertical-align: middle;
+      // background-color: yellowgreen;
+    }
 
+    .product_del_icon {
+      svg {
+        cursor: pointer;
+      }
     }
   }
 
-  #admin_list_chk,
-  #admin_list_num {
+  #admin_list_chk {
     width: 56px;
-    // background-color: red;
+  }
+
+  #admin_list_num {
+    width: 80px;
   }
 
   #admin_list_thumbnail {
@@ -177,6 +201,7 @@ const AdminWrap = styled.main`
   
   #admin_list_date {
     width: 160px;
+    background-color: yellowgreen;
   }
 
   #admin_list_del {
@@ -184,18 +209,32 @@ const AdminWrap = styled.main`
   }
 `
 
-const GoAddProductBtn = styled(BtnBg)`
-  width: 120px;
-  font-size: 16px;
-
-  // background-color: pink;
+const GoAddProductBtn = styled.button`
+  position: absolute;
+  width: 80px;
+  height: 32px;
+  right: 0;
+  font-size: 14px;
 `
 
-const DeleteProductBtn = styled(BtnBg)`
-  width: 120px;
-  font-size: 16px;
+const DeleteProductBtn = styled.button`
+  width: 40px;
+  height: 32px;
+  margin-top: 10px;
+  font-size: 14px;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  background: #F82A2A;
+  cursor: pointer;
 
-  // background-color: pink;
+`
+
+const DeleteChkedBtn = styled.button`
+  width: 80px;
+  height: 32px;
+  margin-top: 10px;
+  font-size: 14px;
 `
 
 
