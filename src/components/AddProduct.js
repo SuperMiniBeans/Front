6aaf07095c5a566034721c920ea6e9d879dcde2c
song-tiigger <applej07@naver.com>
2023-11-24 18:58:35 +0900

    [FEAT] 카테고리 선택 추가

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,44 +1,50 @@
 import styled from "styled-components";
-import { Container, FlexBox } from "../styles/Layout";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { addProductList } from "../store";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
-import { BtnBg } from "../styles/ButtonStyle";
 
-// 카테고리
+import { Container, FlexBox } from "../styles/Layout";
+import { BtnBg } from "../styles/ButtonStyle";
+import { setInputValue, addProductList, selectMajorCategory, selectMinorCategory } from "../store";
+
+
+
+// *********파일 첨부 만들기 ( )***********
+
+
+
 
 function AddProduct() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // 입력값 관리
-  const initialValue = {
-    productName: '',
-    productPrice: 0,
-    discountRate: 0,
-    discountPrice: 0,
-    productSize: '',
-    productColor: '',
-    productQuantity: 0,
-    productExplanation: '',
+  // 카테고리 관련 state 불러와서 사용하기
+  const cateState = useSelector((state) => state.categories);
+  const {majorCategories, minorCategories, selectedMajorCategory, selectedMinorCategory} = cateState;
+
+
+  // major카테고리 선택 상태 업데이트
+  const handleMajorValue = e => dispatch(selectMajorCategory(e.target.value));
+  console.log('셀렉티드대분류', selectedMajorCategory);
+
+  // minor카테고리 선택 상태 업데이트
+  const handleMinorValue = e => dispatch(selectMinorCategory(e.target.value));
+  console.log('셀렉티드소분류', selectedMinorCategory);
+
+  // input관련 state불러와서 사용하기
+  let inputState = useSelector((state) => state.inputValue.inputValues);
+  const {productName, productPrice, discountRate, discountPrice, productSize, productColor, productQuantity, productExplanation} = inputState;
+  // console.log('인풋 스테이트', inputState);
+
+  // input 입력 받은 값 state에저장
+  const handleInputChange = e => {
+    const {name, value} = e.target;
+    dispatch(setInputValue({name, value}));
   }
 
   // 할인 적용 체크 여부
   const [dscntChked, setDscntChked] = useState(false);
-
-  // 사용자 입력값 받기
-  const [inputValues, setInputValues] = useState(initialValue);
-
-  const {productName, productPrice, discountRate,
-  discountPrice, productSize, productColor, productQuantity, productExplanation} = inputValues;
-
-  const onChangeValue = e => {
-    const {value, name: inputName} = e.target;
-    setInputValues({...inputValues, [inputName]: value});
-    console.log(inputValues);
-  }
 
   // 할인 적용 체크
   const handleDscntCheck = e => {
@@ -54,7 +60,7 @@ function AddProduct() {
   // 글 작성 날짜 나타내기
   let date = new Date();
   let year = date.getFullYear();
-  let month = date.getMonth();
+  let month = date.getMonth() + 1;
   let day = date.getDate();
   let today = year + "-" + month + "-" + day;
   // console.log(today);
@@ -64,7 +70,8 @@ function AddProduct() {
     /* 필수 항목을 모두 입력해야 제출 할 수 있도록 유효성 검사 해주기( ) */
 
     axios.post('/registerProduct', {
-      categoryMinorCode: 0,
+      categoryMajorCode: selectedMajorCategory,
+      categoryMinorCode: selectedMinorCategory,
       productNumber: '',
       productName: productName,
       productPrice: productPrice,
@@ -73,16 +80,19 @@ function AddProduct() {
       productSize: productSize,
       productColor: productColor,
       productQuantity: productQuantity,
-      // productExplanation: productExplanation,
+      productExplanation: productExplanation,
       productRegisterDate: today,
     })
       .then(response => {
         console.log(response.data);
         dispatch(addProductList(response.data)); // redux store에 전송한 데이터 추가
-        // navigate('/admin');
+        console.log('성공');
+        navigate('/admin');
       })
       .catch(error => {
         console.log(error);
+        console.log('실패!');
+
       });
   }
 
@@ -92,13 +102,47 @@ function AddProduct() {
         <h2>상품 등록</h2>
 
         <div className="align_center">
-          <form action="" method="POST">
+          <div>날짜: {today}</div>
+
+          <form>
+            <div id="p_cate_box">
+              <AddINputWrap>
+                <label>카테고리</label>
+                <select 
+                  name="categoryMajorCode"
+                  value={selectedMajorCategory ? majorCategories.value : ''}
+                  onChange={handleMajorValue}
+                >
+                  <option>대분류</option>
+                  {majorCategories.map((category) => (
+                    <option key={category.id} value={category.value}>
+                      {category.name}
+                    </option>
+                  ))}
+                </select>
+
+                {selectedMajorCategory && (
+                  <select 
+                    name="categoryMinorCode"
+                    value={selectedMinorCategory || ''}
+                    onChange={handleMinorValue}
+                  >
+                    <option>소분류</option>
+                    {minorCategories[selectedMajorCategory].map((cate) => (
+                      <option key={cate.id} value={cate.value}>
+                        {cate.name}
+                      </option>
+                    ))}
+                  </select>
+                )}
+              </AddINputWrap>
+            </div>
 
             <div id="p_name_box">
               <AddINputWrap>
                 <label>상품명</label>
                 <div className="input_box">
-                  <AddINput type="text" name="productName" value={productName} onChange={onChangeValue}/>
+                  <AddINput type="text" name="productName" value={productName} onChange={handleInputChange}/>
                 </div>
               </AddINputWrap>
             </div> {/* p_name_box */}
@@ -112,7 +156,7 @@ function AddProduct() {
               <AddINputWrap>
                 <label>가격</label>
                 <div className="input_box">
-                  <AddINput type="text" name="productPrice" value={productPrice} onChange={onChangeValue} />
+                  <AddINput type="text" name="productPrice" value={productPrice} onChange={handleInputChange} />
                 </div>
               </AddINputWrap>
 
@@ -127,7 +171,7 @@ function AddProduct() {
                 {/* <AddINputWrap>
                   <label>할인가</label>
                   <div className="input_box">
-                    <AddINput type="text" name="discountPrice" value={dscntChked ? dscntPrice : 0} disabled={dscntChked ? false : true} onChange={calcDscntPrice} />
+                    <AddINput type="text" name="discountPrice" value={dscntChked ? dscntPrice : 0} disabled={dscntChked ? false : true} onChange={handleInputChange} />
                   </div>
                 </AddINputWrap> */}
               </FlexBox>
@@ -138,21 +182,21 @@ function AddProduct() {
               <AddINputWrap>
                 <label>사이즈</label>
                 <div  className="input_box">
-                  <AddINput type="text" name="productSize" value={productSize} onChange={onChangeValue}/>
+                  <AddINput type="text" name="productSize" value={productSize} onChange={handleInputChange}/>
                 </div>
               </AddINputWrap>
 
               <AddINputWrap>
                 <label>색상</label>
                 <div className="input_box">
-                  <AddINput type="text" name="productColor" value={productColor} onChange={onChangeValue} />
+                  <AddINput type="text" name="productColor" value={productColor} onChange={handleInputChange} />
                 </div>
               </AddINputWrap>
 
               <AddINputWrap>
                 <label>재고 수량</label>
                 <div className="input_box">
-                  <AddINput type="text" name="productQuantity" value={productQuantity} onChange={onChangeValue} />
+                  <AddINput type="text" name="productQuantity" value={productQuantity} onChange={handleInputChange} />
                 </div>
               </AddINputWrap>
             </div> {/* p_info_box */}
@@ -160,23 +204,23 @@ function AddProduct() {
             <AddINputWrap>
               <label>상품 설명</label>
               <div className="input_box">
-                <AddTextArea type="text" name="productExplanation" id="explanation" value={productExplanation} onChange={onChangeValue} />
+                <AddTextArea type="text" name="productExplanation" id="explanation" value={productExplanation} onChange={handleInputChange} />
               </div>
             </AddINputWrap>
 
-            <AddINputWrap>
+            {/* <AddINputWrap>
               <label>사이즈 안내</label>
               <div className="input_box">
                 <AddTextArea type="text" name="" id="size_guide" />
               </div>
-            </AddINputWrap>
+            </AddINputWrap> */}
 
-            <AddINputWrap>
+            {/* <AddINputWrap>
               <label>배송 및 <br /> 환불 안내</label>
               <div className="input_box">
                 <AddTextArea type="text" name="" id="shipping_guide" />
               </div>
-            </AddINputWrap>
+            </AddINputWrap> */}
 
             <div>
               <AddProductBtn type="submit" onClick={onAddSubmit}>등록</AddProductBtn>
@@ -206,6 +250,7 @@ const AddProductWrap = styled.div`
     // background-color: #eee;
   }
 
+  #p_cate_box,
   #p_name_box,
   #p_price_box,
   #p_info_box {
@@ -218,14 +263,25 @@ const AddINputWrap = styled.div`
   width: 100%;
   margin-bottom: 10px;
   align-items: center;
+  // background-color: pink;
+
 
   label {
     width: 140px;
+    margin-right: 20px;
     font-size: 20px;
+    // background-color: #eee;
   }
 
   .input_box {
     width: 100%;
+
+    input,
+    textarea {
+      width: calc(780px-140px);
+      // background-color: blue;
+
+    }
   }
 `
 
