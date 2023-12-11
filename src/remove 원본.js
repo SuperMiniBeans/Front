7c6342d7d45e0364261346b12aaa744d078ac188
song-tiigger <applej07@naver.com>
2023-12-11 21:33:07 +0900

    [CHORE]

--- src/remove 원본.js
+++ src/remove 원본.js
@@ -18,6 +18,23 @@
   //     })
   // }
 
+  // 체크한 상품 삭제하기 (axios 요청 여러개 보내기)
+  // const onRemove = () => { // 뭘 삭제할지 말 해줘야함
+  //   window.alert("삭제하시겠습니까?");
+  //   for(let i=0; i<checkedProducts.length; i++){
+  //     axios.get('/productDelete?productNumber='+checkedProducts[i])
+  //       .then(response => {
+  //         console.log(response.data);
+  //         dispatch(removeProductList(checkedProducts));
+  //         window.location.reload();
+  //       })
+  //       .catch(error => {
+  //         console.log(error);
+  //         console.log('체크된 항목?', checkedProducts)
+  //       })
+  //   }
+  // }
+
 
   
   // 객체에서 productNumber와 같은 id 찾아서 데이터 불러오기 ↓
