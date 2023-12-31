3cff5605c2ab4ba179d23c9b6099732937a76ec1
song-tiigger <applej07@naver.com>
2023-12-31 22:36:19 +0900

    [ADD] 아임포트 결제 기능 구현중

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -8,12 +8,75 @@ import { Link, useLocation } from "react-router-dom";
 
 // 디자인 레퍼런스 다시 찾아서 해보기( )
 
+/* 프론트에서 아임포트로 직접요청하지말고 서버(스프링)에서 해야합니다.
+
+받는 서버는 아래처럼 구현해야 합니다.
+
+1. 데이터를 받는다.
+2. 검증요청을 한다
+3. 검증요청 후 결과가 정상이면 결재요청을한다
+4. 결재후 결과값을 프론트에게 되돌려준다.
+
+프론트는 axios 모듈을 활용하여 서버에게 결재를 요청(예: http:x.x.x.x/결재요청) 하고,
+
+서버는 프론트한테 요청 받으면 위 4단계 다 하고나서 결과값을 프론트에게 되돌려주면됩니다.
+
+카카오톡, 네이버 및 구글 같은 소셜로그인, 디지털원패스와 비슷한 개념입니다!
+*/
+
+// 결제 완료 후 이동할 페이지 만들기( )
+
 
 function Order() {
   const dispatch = useDispatch();
   const location = useLocation();
   const { pickedItems } = location.state;
   console.log('order pickedItems', pickedItems);
+
+
+  /* 없어도 되는 부분인가? 나중에 실행해서 확인해보기 (여기부터) */
+  useEffect(() => {
+    const iamport = document.createElement("script");
+    iamport.src = "http://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
+    document.head.appendChild(iamport);
+    return () => {
+      document.head.removeChild(iamport);
+    };
+  }, []);
+  /* 없어도 되는 부분인가? 나중에 실행해서 확인해보기 (여기까지) */
+
+
+  const requestPay = () => {
+    const { IMP } = window;
+    IMP.init('imp28217053');
+
+    // 결제창 호출, { }안에는 결제 데이터 정의 - 이후에 vale 자리에 데이터 바인딩하기 
+    IMP.request_pay({
+      pg: 'kakaopay', //{PG사코드}.{PG상점ID}
+      pay_method: 'card',
+      merchant_uid: new Date().getTime(),
+      name: '결제 테스트',
+      amount: 1004, // 결제 금액
+      buyer_email: 'applej07@naver.com',
+      buyer_name: '송하정',
+      buyer_tel: '010-5113-5826',
+      buyer_addr: '서울특별시',
+      buyer_postcode: '123-456',
+    }, async (res) => {
+      try {
+        const { data } = await axios.post('/' + res.imp_uid); // data: 서버에서 보내준 결과를 data라는 변수에 담는 것/ 서버 주소 뒤의 res.imp_uid는 결제 검증을 위해 요청 url에 보내는 값/ imp_uid는 아임포트 결제 후에 반환되는 response객체 내부에 있는 속성으로 아임포트에서 결제 완료 시 자동으로 생성되는 고유한 결제 ID
+        if (res.paid_amount === data.response.amount) { // 요청 결제 금액이 서버에서 반환하는 금액과 같으면 결제성공!/ data.response.amount에서 amount는 서버 코드에 작성된 변수에 맞추기
+          alert('결제 성공');
+        } else {
+          alert('결제 실패');
+        }
+      } catch (error) {
+        console.error('결제 오류!!!:', error);
+        alert('결제 실패');
+      }
+    });
+  }
+
 
   return(
     <OrderWrap>
@@ -106,7 +169,7 @@ function Order() {
         <section className="payment-section">
           <h3>결제 정보</h3>
           <div>
-            <button>결제하기</button>
+            <button onClick={requestPay}>결제하기</button>
           </div>
         </section>
 
