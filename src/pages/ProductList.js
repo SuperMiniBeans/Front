1da60907b80cbf6a17ba866fe0aea7333a108af1
song-tiigger <applej07@naver.com>
2023-12-31 12:22:40 +0900

    이미지src수정 및 필요 없는 코드 삭제

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,91 +1,43 @@
 import styled from "styled-components";
 import { FlexBoxCol } from "../styles/Layout";
 import { Routes, Route, Link } from "react-router-dom";
-// import { Link } from "react-router-dom";
-import ProductDetail from "./ProductDetail";
-import { useState, useEffect } from "react";
-import axios from "axios";
 
+function ProductList({ products }) {
 
+  console.log('prolist', products);
 
-function ProductList() {
-  /* 상품명, 이미지에 productDetail로 이동할 수 있는 라우터 설정 */
-  /* 상품명, 가격은 데이터를 받아오기 이미지도? */
-
-  const [product, setProduct] = useState({
-    productName: '',
-    productPrice: 0,
-    discountRate: 0,
-    discountPrice: 0,
-    productSize: '',
-    productColor: '',
-    productQuantity: 0,
-    productExplanation: '',
-    productExplanation1: '',
-    productExplanation2: '',
-  })
-  const {productName, productPrice, discountRate, discountPrice, productSize, productColor, productQuantity, productExplanation, productExplanation1, productExplanation2} = product;
-
-  const [imgData, setImgData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post('/fileList', {
-          userId: sessionStorage.getItem("아이디"),
-        })
-        setImgData(response.data);
-        setProduct(response.data);
-        // setMajorCategory(response.data[0].categoryMajorCode);
-        // setMinorCategory(response.data[0].categoryMinorCode);
-      }
-      catch(error) {
-        console.log(error);
-        if(product.length === 0) {
-          console.log('데이터 없음');
-        }
-      }
-    }
-    fetchData();
-  }, []);
-
-
-
-  // 이미지 경로 배열에 담기
-  const [imgPathList, setImgPathList] = useState([]);
-  useEffect(() => {
-    const newImgPathList = imgData.map(item => 
-      `/upload/${item.fileUploadPath}/${item.fileUuid}_${item.fileName}`
-    );
-    setImgPathList(newImgPathList);
-  }, [imgData]); 
-  console.log('imgPathList', imgPathList);
-
-  
   return(
-    <ProductListWrap>
-      <FlexBoxCol>
-        <ImgWrap>
-          <Link to={'/product/list/detail'}><img src={imgPathList[0]} alt="outer" /></Link>
-        </ImgWrap>
-        <Title><Link to={'/product/list/detail'}><h3>{productName}</h3></Link></Title>
-        <PriceWrap>
-          <span className="dscnt_rate">{discountRate}</span>
-          <span className="dscnt_price">{discountPrice}</span>
-          <span className="price">{productPrice}</span>
-        </PriceWrap>
-      </FlexBoxCol>
+    <>
+    {products ? 
+      <div>
+        <FlexBoxCol>
 
-      <Routes>
-        <Route path="/product/list/detail" element={<ProductDetail />} />
-      </Routes>
-    </ProductListWrap>
+          <ImgWrap>
+            <Link to={`/product/list/detail/${products.productNumber}`}>
+              <img src={`/upload/${products.fileUploadPath}/${products.fileUuid}_${products.fileName}`} alt={products.productName} />
+            </Link>
+          </ImgWrap>
+
+          <Title>
+            <Link to={`/product/list/detail/${products.productNumber}`}>
+              <h3>{products.productName}</h3>
+            </Link>
+          </Title>
+
+          <PriceWrap>
+            <span className="dscnt_rate">{products.discountRate}</span>
+            <span className="dscnt_price">{products.discountPrice}</span>
+            <span className="price">{products.productPrice}</span>
+          </PriceWrap>
+
+        </FlexBoxCol>
+      </div>
+      : 
+      <div>데이터 로딩즁</div>
+      }
+    </>
   )
 }
-
-const ProductListWrap = styled.div`
-  // background-color: yellow;
-`
 
 const ImgWrap = styled.div`
   width: 280px;
