e56c2e3c45cf6e372018fabd237c3240d7a26d86
song-tiigger <applej07@naver.com>
2024-01-08 02:43:34 +0900

    [ADD] 상품설명을 탭 메뉴로 구현, 이미지 넘기기 구현

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -8,17 +8,15 @@ import { sendCartItems } from "../store";
 import formatPrice from "../utils/formatPrice";
 
 import { SlBag } from "react-icons/sl"
-import { IoIosHeartEmpty } from "react-icons/io";
-import { IoIosHeart } from "react-icons/io";
+import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
+import { IoChevronForwardSharp, IoChevronBackSharp } from "react-icons/io5";
+
 
 
 /* 
 1. admin이 isLogin===true이면 [수정]버튼 보이게 하기 ( )
 2. [수정] 버튼 클릭하면 상품 수정 페이지로 이동( )
 상품 등록 페이지로 이동하되 기존 데이터는 남아있게
-3. 할인가 표시하기 - 서버에서 받은 데이터 중에서 할인가 있으면 할인가 표시 
-4. 아코디언 메뉴, 사진 넘기기 => 컴포넌트로 작성하기
-
 */
 
 
@@ -41,10 +39,47 @@ function ProductDetail() {
   })
   const {productName, productPrice, discountRate, discountPrice, productSizes, productColors, productQuantity, productExplanation, productExplanation1, productExplanation2} = product;
 
+  const explanationTab = product ? [
+    {
+      id: 0,
+      title: "상품 설명", 
+      content: (
+        <p className="product_guide">
+          {productExplanation}
+        </p>
+      )
+    },
+    {
+      id: 1,
+      title: "사이즈 안내", 
+      content: (
+        <p className="size_guide">
+          {productExplanation1}
+        </p>
+      )
+    },
+    {
+      id: 2,
+      title: "배송 안내", 
+      content: (
+        <p className="shipping_guide">
+          {productExplanation2}<br />
+          배송 방법 : 택배
+          배송 지역 : 전국지역
+          배송 기간 : 3일 ~ 7일
+          배송 안내 :
+          고객님께서 주문하신 상품은 입금 확인후 배송해 드립니다. 다만, 상품종류에 따라서 상품의 배송이 다소 지연될 수 있습니다.
+        </p>
+      )
+    },
+  ] : [];
+  const [index, setIndex] = useState(0);
+
+
   // 이미지 데이터 배열로 저장
   const [imgData, setImgData] = useState([]);
 
-  /* db에 저장된 상품 데이터 불러오기(비동기) */
+  /* db에 저장된 상품 데이터 불러오기 */
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -56,6 +91,8 @@ function ProductDetail() {
         sessionStorage.setItem("productNumber", response.data[0].productNumber);
         // setMajorCategory(response.data[0].categoryMajorCode);
         // setMinorCategory(response.data[0].categoryMinorCode);
+        console.log('response.data', response.data);
+
       }
       catch(error) {
         console.log(error);
@@ -194,6 +231,86 @@ function ProductDetail() {
     }));
   };
 
+  // 개별 상품 주문하기 
+  const [checkedProducts, setCheckedProducts] = useState([]);
+
+  const handelOrderEach = async (id) => {
+    const clickOrderEach = [...checkedProducts, id];
+    setCheckedProducts(clickOrderEach);
+
+    try {
+      const pickCartRes = await axios.post('/pickCart', {
+        cartNumber: clickOrderEach,
+      });
+  
+      const allCartPriceRes = await axios.post('/allCartPrice', {
+        cartNumber: clickOrderEach,
+      });
+      console.log('allCartPrice', allCartPriceRes.data);
+  
+      navigate('/order', {
+        state: { 
+          pickedItems: pickCartRes.data, 
+          totalPrice: allCartPriceRes.data.discountTotalPrice,
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    console.log('checkedProducts', checkedProducts)
+  }
+
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  // 좋아요 처리 함수
+  const handleLike = async () => {
+    try {
+      const res = await axios.post('/like', {
+        userNumber: sessionStorage.getItem("userNumber"),
+        productNumber: id,
+      });
+      console.log(res.data);
+      // API 호출이 성공하면 상태를 변경합니다.
+      setIsLiked(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 좋아요 취소 처리 함수
+  const handleUnlike = async () => {
+    try {
+      const res = await axios.post('/dislike', {
+        userNumber: sessionStorage.getItem("userNumber"),
+        productNumber: id,
+      });
+      console.log(res.data);
+      // API 호출이 성공하면 상태를 변경합니다.
+      setIsLiked(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [mainImg, setMainImg] = useState(""); // mainImg의 초기값을 빈 문자열로 설정합니다.
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    if (imgPathList.length > 0) { // imgPathList에 이미지가 있을 때만 mainImg를 업데이트합니다.
+      setMainImg(imgPathList[imageIndex]);
+    }
+  }, [imgPathList, imageIndex]); // imgPathList의 상태가 변경될 때마다 이 효과를 실행합니다.
+
+  const handlePrevClick = () => {
+    setImageIndex(prevIndex => prevIndex - 1 < 0 ? imgPathList.length - 1 : prevIndex - 1);
+  };
+
+  const handleNextClick = () => {
+    setImageIndex(prevIndex => prevIndex + 1 === imgPathList.length ? 0 : prevIndex + 1);
+  };
+
+
   return(
     <ProductDetailWrap>
       <Container>
@@ -220,11 +337,26 @@ function ProductDetail() {
           <div className="left">
             <ThumbWrap className="detail_img_wrap">
               <div className="main_img">
-                <img id="admin_thumb_img" src={`${imgPathList[0]}`} alt="thumbnail" />
+                <img id="admin_thumb_img" src={mainImg} alt="thumbnail" />
+                
+                <CarouselButton onClick={handlePrevClick}>
+                  <IoChevronBackSharp size={40} />
+                </CarouselButton>
+
+                <CarouselButton onClick={handleNextClick}>
+                  <IoChevronForwardSharp size={40} />
+                </CarouselButton>
               </div>
+
               <div className="next_img_wrap">
                 {imgPathList.map((img, index) => (
-                  <img src={`${img}`} key={index} alt="등록한 이미지"/>
+                  <img 
+                    src={`${img}`} 
+                    key={index} 
+                    alt="등록한 이미지"
+                    onMouseOver={() => setImageIndex(index)}
+                    className={imageIndex === index ? 'selected' : ''} 
+                  />
                 ))}
               </div>
             </ThumbWrap>
@@ -263,7 +395,7 @@ function ProductDetail() {
                 <select
                   value={selectedColor || ""}
                   onChange={handleSelectedColor}
-                  >
+                >
                   <option value="">색상 선택</option>
                   {selectedSize && productColors.map((color) => (
                     <option key={color} value={color}>
@@ -273,22 +405,22 @@ function ProductDetail() {
                 </select>
 
                 <SelectedOptionBox>
-                    <ul>
-                      {selectedOptions.map((option, index) => (
-                        <li key={index}>
-                          <span id="option_text">{option.text}</span>
-                          <div className="handle_quantity_box">
-                            <button id="minus_btn" onClick={() => minus(index)}></button>
-                            <span id="option_quantity">{option.quantity ? option.quantity : 1}</span>
-                            <button id="plus_btn" onClick={() => plus(index)}></button>
-                          </div>
-                          <span id="option_cal_price">{formatPrice(calEachPrice(index))}원</span>
-                          <div id="remove_btn_box">
-                            <button id="remove_btn" onClick={() => removeOption(index)}></button>
-                          </div>
-                        </li>    
-                      ))}
-                    </ul>
+                  <ul>
+                    {selectedOptions.map((option, index) => (
+                      <li key={index}>
+                        <span id="option_text">{option.text}</span>
+                        <div className="handle_quantity_box">
+                          <button id="minus_btn" onClick={() => minus(index)}></button>
+                          <span id="option_quantity">{option.quantity ? option.quantity : 1}</span>
+                          <button id="plus_btn" onClick={() => plus(index)}></button>
+                        </div>
+                        <span id="option_cal_price">{formatPrice(calEachPrice(index))}원</span>
+                        <div id="remove_btn_box">
+                          <button id="remove_btn" onClick={() => removeOption(index)}></button>
+                        </div>
+                      </li>    
+                    ))}
+                  </ul>
                 </SelectedOptionBox>
                 <div className="option_total_box">
                   <div>총 상품 금액</div>
@@ -297,34 +429,42 @@ function ProductDetail() {
               </SelectBox> {/* select_box */}
 
               <BuyBtnWrap className="btn_wrap">
-                <button id="buy_btn">바로구매</button>
-                <button id="add_cart_btn" onClick={handleAddToCart}><SlBag size={24}/></button>
-                <button id="add_wish_btn"><IoIosHeartEmpty size={28} /></button>
+                <button id="buy_btn" onClick={() => handelOrderEach()}>
+                  바로구매
+                </button>
+                <button id="add_cart_btn" onClick={handleAddToCart}>
+                  <SlBag size={24}/>
+                </button>
+                <button id="add_wish_btn" onClick={isLiked ? handleUnlike : handleLike}>
+                  {isLiked ? <IoIosHeart size={28}/> : <IoIosHeartEmpty size={28} />}
+                  
+                </button>
               </BuyBtnWrap>
 
+              <ExplanationTab>
+                <div className="tab_menu_title_wrap">
+                  <ul>
+                    {explanationTab.map(item => (
+                      <li 
+                        key={item.id} 
+                        className={index === item.id ? 'active' : null}
+                        onClick={() => setIndex(item.id)}
+                      >
+                        {item.title}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
 
-              {/* 아코디언 메뉴 참고 -> 간단하게 이미지 클릭하면 state를 변경시켜 해당 메뉴 스타일 display none 에서 블락으로 변경만 시켜주면 될것 같습니다 */}
-              <div className="explanation">
-                <p>
-                  {productExplanation}
-                  {/* 상품 설명입니다. 소재, 디자인 포인트, 활용 방법 등에 관해 설명하면 됩니다. 상품 설명입니다. 소재, 디자인 포인트, 활용 방법 등에 관해 설명하면 됩니다.상품 설명입니다. 소재, 디자인 포인트, 활용 방법 등에 관해 설명하면 됩니다.상품 설명입니다. 소재, 디자인 포인트, 활용 방법 등에 관해 설명하면 됩니다.상품 설명입니다. 소재, 디자인 포인트, 활용 방법 등에 관해 설명하면 됩니다.상품 설명입니다. 소재, 디자인 포인트, 활용 방법 등에 관해 설명하면 됩니다.상품 설명입니다. 소재, 디자인 포인트, 활용 방법 등에 관해 설명하면 됩니다. */}
-                </p>
-              </div>
-              <div className="size_guide">
-                <p>
-                  {productExplanation1}
-                  {/* 사이즈 가이드 설명 쓰세요<br />
-                  상세 사이즈 표시하기 <br /> */}
-                </p>
-              </div>
-              <div className="shipping_guide">
-                <p>
-                  {productExplanation2}
-                  {/* 배송 및 환불 안내 <br />
-                  배송은 어쩌구 저쩌구 <br />
-                  환불은 이래이래 저래저래 */}
-                </p>
-              </div>
+                <div className="tab_menu_content_wrap"> 
+                  {/* p map돌리기 */}
+                  {explanationTab.filter(item => index === item.id).map(item => (
+                    <div key={index} className="tab_con_box">{item.content}</div>
+                  ))}
+                </div>
+              </ExplanationTab>
+
+              
             </InfoWrap> {/* detail_infos_wrap */}
           </div> {/* right */}
         </FlexBox>
@@ -381,7 +521,10 @@ const CtgryWrap = styled.div`
 `
 
 const ThumbWrap = styled.div`
+  position: relative;
+
   .main_img {
+    position: relative;
     width: 580px;
     height: 704px;
     margin-bottom: 10px;
@@ -404,9 +547,37 @@ const ThumbWrap = styled.div`
       margin-right: 10px;
       object-fit: cover;
       cursor: pointer;
+
+      &.selected {
+        border: 1px solid #333;
+      }
     }
   }
 `
+
+const CarouselButton = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 0 10px;
+  background: rgba(255, 255, 255, 0.3);
+  border: none;
+  cursor: pointer;
+
+  svg {
+    width: 20px;
+    height: 80px;
+    color: #333;
+  }
+
+  &:first-of-type {
+    left: 0;
+  }
+
+  &:last-child {
+    right: 0;
+  }
+`;
 
 const InfoWrap = styled.div`
   h3 {
@@ -625,6 +796,7 @@ const SelectedOptionBox = styled.div`
 const BuyBtnWrap = styled.div`
   display: flex;
   gap: 4px;
+  margin-bottom: 100px;
 
   #buy_btn,
   #add_cart_btn,
@@ -666,6 +838,52 @@ const BuyBtnWrap = styled.div`
       color: #F82A2A;
     }
   }
+`
+
+const ExplanationTab = styled.div`
+  // background: pink;
+
+  .tab_menu_title_wrap {
+
+    ul {
+      display: flex;
+      justify-content: space-between;
+
+      li {
+        flex: 1;
+        padding: 4px 0;
+        font-size: 14px;
+        text-align: left;
+        color: #aaa;
+        border-bottom: 1px solid #aaa;
+        cursor: pointer;
+        // background: yellowgreen;
+
+        &:nth-child(2) {
+          margin: 0 4px;
+        }
+
+        &.active {
+          color: #333;
+          border-bottom: 1px solid #333;  
+        }
+
+      }
+    } //ul
+  } //tab_menu_title_wrap
+
+  .tab_menu_content_wrap {
+
+    .tab_con_box {
+      p {
+        min-height: 200px;
+        padding: 20px 10px;
+        font-size: 14px;
+        line-height: 20px;
+        // background: #eee;
+      }
+    } //tab_con_box
+  } //tab_menu_content_wrap
 `
 
 export default ProductDetail;
